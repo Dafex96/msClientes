f87383f9f754e2c7b55837b62e74e049b5417658
Dafex96 <davicolo717@gmail.com>
2026-06-23 16:57:44 -0400

    minor fix

--- src/main/java/cl/duoc/msClientes/dto/ClienteDTO.java
+++ src/main/java/cl/duoc/msClientes/dto/ClienteDTO.java
@@ -1,5 +1,6 @@
 package cl.duoc.msClientes.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +8,19 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Representa un clienteDTO en el sistema")
 public class ClienteDTO {
 
+    @Schema(description = "ID del cliente")
     private Integer id;
+    
+    @Schema(description = "Nombre del cliente")
     private String nombre;
+    
+    @Schema(description = "Correo electrónico del cliente")
     private String correo;
+
+    @Schema(description = "Teléfono del cliente")
     private String telefono;
 
 }
